cb1b9da476a5f7df26fabf7ef048f425e343e8ad
rbaker2019 <49624583+rbaker2019@users.noreply.github.com>
2019-12-17 16:35:19 -0600

    Add simple static burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Aux from '../../hoc/Auxillary';
 
+import Burger from '../../components/Burger/Burger';
+
 // This will be converted to a functional component with hooks
 class BurgerBuilder extends React.Component {
 
     render() {
         return (
             <Aux>
-                <div>Burger</div>
+                <Burger />
                 <div>Build Controls</div>
             </Aux>
         );
