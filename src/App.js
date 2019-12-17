b689b9c1bd104efd1a7d17a76cc2a110abed7279
rbaker2019 <49624583+rbaker2019@users.noreply.github.com>
2019-12-17 14:24:12 -0600

    154. Creating a Layout Component - basic scaffolding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import Layout from './components/Layout/Layout';
+
 class App extends React.Component {
 
   render() {
     return (
       <div>
-        
+        <Layout>
+          <p>Test</p>
+        </Layout>
       </div>
     );
   }
