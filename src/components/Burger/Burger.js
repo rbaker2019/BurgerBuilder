e527e233c203b03a4eb5ad42fd0ebfdc7e202406
rbaker2019 <49624583+rbaker2019@users.noreply.github.com>
2019-12-17 21:22:50 -0600

    Burger.js - handle 0 ingredients case

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,14 +6,21 @@ import classes from './Burger.module.css';
 const burger = (props) => {
 
     // convert object of ingredients:count pairs to array of JSX elements
-    const transformedIngredients =
+    let transformedIngredients =
         Object.keys(props.ingredients)
             .map(ingredientKey => {
                 return [...Array(props.ingredients[ingredientKey])]
                     .map((_, i) => {
                         return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
                     });
-            });
+            })
+            .reduce((arr, el) => {
+                return arr.concat(el);
+            }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please add ingredients</p>;
+    }
 
     return (
         <div className={classes.Burger}>
