3ee0d2265fdf4dda86a9834eae2a3864482feebb
rbaker2019 <49624583+rbaker2019@users.noreply.github.com>
2020-01-28 14:29:55 -0600

    Style logo component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import classes from './Logo.module.css';
 
 // import image to make webpack aware of your files, instead of using relative links
 import burgerLogo from '../../assets/images/Logo.png';
 
 const logo = (props) => {
     return (
-        <div>
+        <div className={classes.Logo}>
             <img src={burgerLogo} />
         </div>
     );
